b2392de8204adb5727de80b6b32ca83c68c66b4a
JiaZheng03 <wongkz98@gmail.com>
2026-07-19 19:51:15 +0800

    Initial commit: add ChainCargo project

---
+++ ChainCargo/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>ChainCargo</h1>
+      <p>Decentralized Escrow and Milestone-Based Logistics Platform</p>
+    </div>
+  );
+}
+
+export default App;
